c22bac169e495fdaec34c6816290e86ad9d42ed3
vitinho6 <joao.vitor.figueiredo@ccc.ufcg.edu.br>
2020-07-02 20:19:11 -0300

    adicionando pecas ao tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,16 +11,21 @@ public class ChessMatch {
 
     public ChessMatch() {
         board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces(){
         ChessPiece[][] auxMat = new ChessPiece[board.getRows()][board.getColumns()];
-        for (int i = 0; i<board.getRows(); i++){
-            for(int j = 0; j<board.getColumns(); j++){
-                 auxMat[i][j] = (ChessPiece) board.piece(i, j);
+        for (int i = 0; i<board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
+                auxMat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
-
         return auxMat;
+    }
+
+    public void initialSetup(){
+        board.placePiece(new Rook(board, Color.BLACK), new Position(2,1));
+        board.placePiece(new King(board, Color.BLACK), new Position(3,4));
     }
 }
