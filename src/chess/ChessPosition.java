450ff901fc00e14d4c13827117a5426e0eb40030
vitinho6 <joao.vitor.figueiredo@ccc.ufcg.edu.br>
2020-07-03 19:24:48 -0300

    Colorindo impressao do tabuleiro

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -15,6 +15,15 @@ public class ChessPosition {
         this.row = row;
     }
 
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
     protected Position toPosition(){
         return new Position(8-row, column - 'a');
     }
